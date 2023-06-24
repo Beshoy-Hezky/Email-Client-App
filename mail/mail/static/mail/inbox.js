365cdbf3f6dee98d382c599ca88bb1a9c193a478
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-06-24 12:18:13 -0400

    F01: Complete email sending mechanism

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('in box'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  // sending the emails
+  document.querySelector('#compose-form').onsubmit = sendEmail
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +32,28 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function sendEmail(){
+  event.preventDefault();
+  console.log("SEND");
+
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+      .then(response => response.json())
+      .then(result => {
+        // Print result
+        console.log(result);
+        load_mailbox('sent');
+      });
 }
